8b13206ef77b64a1a6e14090965cb9b977ea5783
afonsosgsramalho <afonsosgsramalho@gmail.com>
2025-04-12 01:24:33 +0100

    Add exceptions class

---
+++ autoschema/exceptions.py
@@ -0,0 +1,7 @@
+class DataExtractionError(Exception):
+    """Custom exception for data extraction failures."""
+    pass
+
+class ModelTypeError(Exception):
+    """Custom exception for data extraction failures."""
+    pass
